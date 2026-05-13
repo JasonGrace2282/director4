90e222c05b95daff1c2ee4861d34e1732ff14c43
JasonGrace2282 <aarush.deshpande@gmail.com>
2026-05-13 09:04:27 -0400

    make it a decorator

--- manager/director/apps/sites/admin.py
+++ manager/director/apps/sites/admin.py
@@ -17,7 +17,7 @@ from .models import (
     SiteResourceLimits,
 )
 
-admin.register(DockerImage)
+@admin.register(DockerImage)
 class DockerImageAdmin(admin.ModelAdmin):
     list_display = ('name', 'friendly_name', 'description')
     search_fields = ('name', 'friendly_name', 'description')
